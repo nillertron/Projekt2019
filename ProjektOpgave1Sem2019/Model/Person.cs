0656e2cac3f6319ad891ca268eed5aefefa9aaff
Nichlas Christensen <nichlaschristensen@live.dk>
2019-11-25 13:12:00 +0100

    lc

---
+++ ProjektOpgave1Sem2019/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektOpgave1Sem2019.Model
+{
+    public abstract class Person
+    {
+        public int Id { get; }
+        public string Navn { get; private set; }
+        public string Efternavn { get; private set; }
+        public string TelefonNr { get; private set; }
+        public string KontoNr { get; private set; }
+        public Person(int Id, string Navn, string Efternavn, string TelefonNr, string KontoNr)
+        {
+            this.Id = Id;
+            this.Navn = Navn;
+            this.Efternavn = Efternavn;
+            this.TelefonNr = TelefonNr;
+            this.KontoNr = KontoNr;
+        }
+    }
+}
---
+++ ProjektOpgave1Sem2019/View Model/Form1ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektOpgave1Sem2019
+{
+    class Form1ViewModel
+    {
+    }
+}
