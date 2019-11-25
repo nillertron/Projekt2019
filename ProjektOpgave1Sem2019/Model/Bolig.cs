d8acc918a58e290d72825ea7b45aa93f26839d7c
karl mogensen <karlmogensen@hotmail.com>
2019-11-25 14:01:56 +0100

    Køber, Sælger, Ejendomsmægler og Bolig klasser oprettet kun med properties

---
+++ ProjektOpgave1Sem2019/Model/Bolig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektOpgave1Sem2019.Model
+{
+    class Bolig
+    {
+        public int ID { private set; get; }
+        public string Adresse { private set; get; }
+
+        public double Pris { private set; get; }
+
+        public int SælgerID { private set; get; }
+
+        public int Kvm { private set; get; }
+
+        public DateTime OprettelsesDato { private set; get; }
+
+        public int EjendomsmæglerID { private set; get; }
+
+        public int PostNr { private set; get; }
+
+        public Bolig(int ID, string Adresse,double Pris, int SælgerID, int Kvm, DateTime OprettelsesDato, int EjendomsmæglerID, int PostNr)
+        {
+            this.ID = ID;
+            this.Adresse = Adresse;
+            this.Pris = Pris;
+            this.SælgerID = SælgerID;
+            this.Kvm = Kvm;
+            this.OprettelsesDato = OprettelsesDato;
+            this.EjendomsmæglerID = EjendomsmæglerID;
+            this.PostNr = PostNr;
+        }
+       
+    }
+}
---
+++ ProjektOpgave1Sem2019/Model/Sælger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektOpgave1Sem2019.Model
+{
+    class Sælger : Person 
+    {
+        public Sælger(int Id, string Navn, string Efternavn, string TelefonNr, string KontoNr) : base(Id, Navn, Efternavn, TelefonNr, KontoNr)
+        {
+
+        }
+    }
+}
