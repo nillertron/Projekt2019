80b8783de467569e36e1b42b827b03273849a2c5
Martin Schmidt Sørensen <mart95s9@edu.ucl.dk>
2019-11-25 14:02:46 +0100

    SqlConnection added

--- ProjektOpgave1Sem2019/Model/DBHelper.cs
+++ ProjektOpgave1Sem2019/Model/DBHelper.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace ProjektOpgave1Sem2019.Model
 {
     class DBHelper
     {
+        private static string connString = "Data Source = DESKTOP-TEM8DQC\\MARTINSQL; Initial Catalog = RealBoligDB; Integrated Security = SSPI";
+        private static SqlConnection conn = new SqlConnection(connString);
     }
 }
