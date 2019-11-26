05574ddeb6a3b6b8dd861c1fd26ce77cc718c78d
Nichlas Christensen <nichlaschristensen@live.dk>
2019-11-26 10:39:51 +0100

    k

--- ProjektOpgave1Sem2019/View Model/Ejendomsmægler/Form1ViewModel.cs
+++ ProjektOpgave1Sem2019/View Model/Ejendomsmægler/Form1ViewModel.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
+//Nichlas
 namespace ProjektOpgave1Sem2019
 {
     class Form1ViewModel
@@ -17,19 +18,37 @@ namespace ProjektOpgave1Sem2019
 
         }
 
-        public async Task GetEjendomsmæglere()
+        public void GetAll()
         {
+            
             EjendomsmæglerListe = EjendomsmæglerTabelDB.GetAllEjendomsmæglere();
         }
 
-        public async Task OpretEjendomsmægler()
+        public void Edit(Ejendomsmægler e)
         {
-            var d = "16-04-1994";
-            DateTime dato = new DateTime();
-            dato = Convert.ToDateTime(d);
+            bool succes = EjendomsmæglerTabelDB.Update(e);
+            if (succes)
+                EjendomsmæglerListe.ForEach(o => { if (o.Id == e.Id) o = e; });
+        }
 
-            var e1 = new Ejendomsmægler("bob", "bobsen", "88888888", "1234567",dato);
-             EjendomsmæglerTabelDB.OpretEjendomsmægler(e1);
+        public void Delete(Ejendomsmægler e)
+        {
+            bool succes = EjendomsmæglerTabelDB.Delete(e);
+            if (succes)
+                EjendomsmæglerListe.Remove(e);
+            else
+                MessageBox.Show("Error, try again");
+        }
+
+        public void Opret(Ejendomsmægler e)
+        {
+            bool succes = EjendomsmæglerTabelDB.Create(e);
+            if (succes)
+                EjendomsmæglerListe.Add(e);
+            else
+                MessageBox.Show("Error, try again");
+              
+
         }
     }
 }
