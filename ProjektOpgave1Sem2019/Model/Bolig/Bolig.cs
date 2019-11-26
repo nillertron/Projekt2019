b9005e2f93a75759e20132ac5078b7466edfb488
Nichlas Christensen <nichlaschristensen@live.dk>
2019-11-26 11:51:17 +0100

    Bolig input validering

--- ProjektOpgave1Sem2019/Model/Bolig/Bolig.cs
+++ ProjektOpgave1Sem2019/Model/Bolig/Bolig.cs
@@ -9,20 +9,23 @@ namespace ProjektOpgave1Sem2019.Model
     class Bolig
     {
         private int _ID;
-        public int ID { private set { if (value > 0) _ID = value;   } get { return _ID; } }
+        public int ID { private set { if (value > 0) _ID = value; else throw new FormatException("ID skal være over 0"); } get { return _ID; } }
         public string Adresse { private set; get; }
         private double _Pris;
-        public double Pris { private set { if (value > 0) _Pris = value; } get { return _Pris; } }
+        public double Pris { private set { if (value > 0) _Pris = value; else throw new FormatException("Pris skal være over 0"); } get { return _Pris; } }
+        private int _SælgerID;
+        public int SælgerID { private set { if (value > 0) _SælgerID = value; else throw new FormatException("ID skal være over 0"); } get { return _SælgerID; } }
+        private int _Kvm;
+        public int Kvm { private set { if (value > 0 && value < 2000) _Kvm = value; else throw new FormatException("Kvm skal være mellem 1 og 2000"); } get { return _Kvm; } }
 
-        public int SælgerID { private set; get; }
+        private DateTime _OprettelsesDato;
+        public DateTime OprettelsesDato { private set { if (value.Year > 2015) _OprettelsesDato = value; else throw new FormatException("Oprettelsesåret minimum er 2015"); } get { return _OprettelsesDato; } }
 
-        public int Kvm { private set; get; }
+        private int _EjendomsmæglerID;
+        public int EjendomsmæglerID { private set { if (value > 0) _EjendomsmæglerID = value; else throw new FormatException("ID skal være størrere end 0"); } get { return _EjendomsmæglerID; }}
 
-        public DateTime OprettelsesDato { private set; get; }
-
-        public int EjendomsmæglerID { private set; get; }
-
-        public int PostNr { private set; get; }
+        private int _PostNr;
+        public int PostNr { private set { if (value > 500 && value < 10000) _PostNr = value; } get { return _PostNr; } }
 
         public Bolig(int ID, string Adresse,double Pris, int SælgerID, int Kvm, DateTime OprettelsesDato, int EjendomsmæglerID, int PostNr)
         {
