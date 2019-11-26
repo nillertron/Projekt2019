ff49bb0c1d209c5360ebc3ee820a54f97e13db8c
Nichlas Christensen <nichlaschristensen@live.dk>
2019-11-26 10:58:26 +0100

    kd

--- ProjektOpgave1Sem2019/View Model/Ejendomsmægler/Form1ViewModel.cs
+++ ProjektOpgave1Sem2019/View Model/Ejendomsmægler/Form1ViewModel.cs
@@ -10,45 +10,12 @@ namespace ProjektOpgave1Sem2019
 {
     class Form1ViewModel
     {
-        public List<Ejendomsmægler> EjendomsmæglerListe;
         public Form1ViewModel(Form1 form)
         {
-            EjendomsmæglerListe = new List<Ejendomsmægler>();
 
 
         }
 
-        public void GetAll()
-        {
-            
-            EjendomsmæglerListe = EjendomsmæglerTabelDB.GetAllEjendomsmæglere();
-        }
-
-        public void Edit(Ejendomsmægler e)
-        {
-            bool succes = EjendomsmæglerTabelDB.Update(e);
-            if (succes)
-                EjendomsmæglerListe.ForEach(o => { if (o.Id == e.Id) o = e; });
-        }
-
-        public void Delete(Ejendomsmægler e)
-        {
-            bool succes = EjendomsmæglerTabelDB.Delete(e);
-            if (succes)
-                EjendomsmæglerListe.Remove(e);
-            else
-                MessageBox.Show("Error, try again");
-        }
-
-        public void Opret(Ejendomsmægler e)
-        {
-            bool succes = EjendomsmæglerTabelDB.Create(e);
-            if (succes)
-                EjendomsmæglerListe.Add(e);
-            else
-                MessageBox.Show("Error, try again");
-              
-
-        }
+        
     }
 }
