32beb65e875dc2ac49e741bb8f8fd9cd44e15abb
karl mogensen <karlmogensen@hotmail.com>
2019-11-28 09:28:42 +0100

    f

--- ProjektOpgave1Sem2019/View/Bolig/BoligForm.Designer.cs
+++ ProjektOpgave1Sem2019/View/Bolig/BoligForm.Designer.cs
@@ -34,20 +34,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(231, 101);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(308, 124);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.Size = new System.Drawing.Size(71, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "gregreerg";
             // 
             // BoligForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.label1);
             this.Location = new System.Drawing.Point(200, 116);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "BoligForm";
-            this.Size = new System.Drawing.Size(686, 429);
+            this.Size = new System.Drawing.Size(915, 528);
             this.Load += new System.EventHandler(this.BoligView_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
