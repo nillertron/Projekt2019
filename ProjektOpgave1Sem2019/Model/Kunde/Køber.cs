3f4987dd16b99360108234fba2fb7fdd9cf4e61a
Martin Schmidt Sørensen <mart95s9@edu.ucl.dk>
2019-12-03 09:18:54 +0100

    df

--- ProjektOpgave1Sem2019/Model/Kunde/Køber.cs
+++ ProjektOpgave1Sem2019/Model/Kunde/Køber.cs
@@ -20,11 +20,11 @@ namespace ProjektOpgave1Sem2019.Model
         {
 
         }
-        //Martin
-        public Køber(string Navn, string Efternavn, string TelefonNr, string KontoNr, int PostNr, string Adresse):base(Navn, Efternavn, TelefonNr, KontoNr, PostNr, Adresse)
-        {
+        ////Martin
+        //public Køber(string Navn, string Efternavn, string TelefonNr, string KontoNr, int PostNr, string Adresse):base(Navn, Efternavn, TelefonNr, KontoNr, PostNr, Adresse)
+        //{
 
-        }
+        //}
 
         public override void SendContract(string FP, SolgtBolig b)
         {
